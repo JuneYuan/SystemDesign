18f5a1fd4d63bc107891377f8505f49016992ec7
smartjune <yuanzhongyan@gmail.com>
2016-12-01 11:08:07 +0800

    Lint P500 Inverted Index: fixed bugs
    同一个单词在同一个文件中重复出现，该文件的 id 应该只在相应的 Value 中出现一次；
    处理 split() 分割出来的某个单词为 "" 的情况（直接跳过）。

--- src/crawler_typeahead/InvertedIndex.java
+++ src/crawler_typeahead/InvertedIndex.java
@@ -1,33 +1,36 @@
 package crawler_typeahead;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class InvertedIndex {
-    /**
-     * @param docs a list of documents
-     * @return an inverted index
-     */
+	
     public Map<String, List<Integer>> invertedIndex(List<Document> docs) {
         // Write your code here
         if (docs == null || docs.size() == 0)  return null;
         Map<String, List<Integer>> result = new HashMap<>();
         for (Document doc : docs) {
             String[] words = doc.content.split("\\s");
-            for (String word : words) {
-                if (!result.containsKey(word)) {
+            Arrays.sort(words);
+            for (int i = 0; i < words.length; i++) {
+                if (words[i].length() == 0)
+                    continue;
+                if (i > 0 && words[i].equals(words[i - 1]))
+                    continue;
+                    
+                if (!result.containsKey(words[i])) {
                     List<Integer> list = new ArrayList<>();
-                    result.put(word, list);
+                    result.put(words[i], list);
                 }
-                result.get(word).add(doc.id);
+                result.get(words[i]).add(doc.id);
             }
         }
         
         return result;
     }
-
 }
 
 class Document {
